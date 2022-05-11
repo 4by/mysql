bcb7a8e55bdeb0fb3dea3f1f858dd75d3bedfe20
idoodi <idoodi@ya.ru>
2022-05-11 17:21:49 +0300

    1

--- lr3/someProc.sql
+++ lr3/someProc.sql
@@ -1,11 +1,17 @@
+source lr1/starter.sql
+
 delimiter //
 
-CREATE procedure if not exists addTo50 ()
+use monoTableBD//
+
+CREATE procedure if not exists addTo ()
 begin
 declare personNum int default (select count(*) from person);
 declare needToAdd int default (50 - personNum);
 call addPersonArea(needToAdd);
 end//
+
+use multTableBD//
 
 CREATE procedure if not exists addConstaints ()
 begin
@@ -15,12 +21,20 @@ alter table serving modify cost decimal(15,2) unique;
 alter table incident modify actionid int check(actionid>0);
 end//
 
+
+CREATE procedure if not exists fillWithDepr ()
+begin
+insert into person (name) values(null);
+insert into serving(house_id,cost) values(1,(select cost from (select cost from serving where id=1)d));
+insert into incident(house_id, serving_id, actionid) values (1,1, -3);
+end//
+
 delimiter ;
 
 use monoTableBD;
 
 -- задание а
-call addTo50();
+call addTo();
 
 -- задание б
 select id from person where id = 3;
@@ -46,4 +60,6 @@ call addConstaints();
 -- нельзя включить/отключить эти ограничения, только удалить и поставить заново
 
 -- задание з
+-- call fillWithDepr();
 
+-- задание и
